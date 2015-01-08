1bd2e090134d0be5a76ff2c62e54aed761da437c
Manuel Morales <manuelmorales@gmail.com>
2015-01-08 22:19:32 +0000

    Remarkable inspect

---
+++ lib/remarkable_inspect.rb
@@ -0,0 +1,41 @@
+module MiniObject
+  module RemarkableInspect
+    def to_s
+      "< #{self.class.name} : #{remarkable_methods.join(", ")} >"
+    end
+
+    alias inspect to_s
+
+    def self.included klass
+      klass.extend ClassMethods
+    end
+
+    def remarkable_methods
+      self.class.remarkable_methods
+    end
+
+    module ClassMethods
+      def inspect
+        "#{formatted_name}( #{remarkable_methods.join(", ")} )"
+      end
+
+      def remarkable_methods
+        (self.instance_methods - Box.instance_methods).map(&:to_s).tap do |mm|
+          # Substittues [my_method, my_method=] by [my_method/=]
+          mm.grep(/\=$/).each do |setter|
+            getter = setter.gsub /\=$/, ''
+            if mm.include? getter
+              mm.delete setter
+              mm[mm.find_index(getter)] = setter.gsub /\=$/, '/='
+            end
+          end
+        end
+      end
+
+      def formatted_name
+        first_ancestor_name = ancestors.map(&:name).compact.first
+        name || "#{first_ancestor_name}:0x#{'%x' % (object_id << 1)}"
+      end
+    end
+  end
+end
---
+++ spec/remarkable_inspect_spec.rb
@@ -0,0 +1,43 @@
+require_relative 'spec_helper'
+
+RSpec.describe 'RemarkableInspect' do
+  subject { SubjectClass.new }
+
+  before do
+    stub_const 'SubjectClass', Class.new {
+      include RemarkableInspect
+
+      def config; end
+      def config=; end
+      def stores; end
+    }
+  end
+
+  describe '#to_s' do
+    it 'contains class name and class specific methods' do
+      expect(subject.to_s).to eq('< SubjectClass : config/=, stores >')
+    end
+  end
+
+  describe '#inspect' do
+    it 'contains class name and class specific methods' do
+      expect(subject.inspect).to eq('< SubjectClass : config/=, stores >')
+    end
+  end
+
+  describe '.to_s' do
+    it 'contains just the class name' do
+      expect(SubjectClass.to_s).to eq('SubjectClass')
+    end
+  end
+
+  describe '.inspect' do
+    it 'contains class name and class specific methods' do
+      expect(SubjectClass.inspect).to eq('SubjectClass( config/=, stores )')
+    end
+
+    it 'contains the object hash in anonymous classes' do
+      expect(Class.new(SubjectClass).inspect).to match(/SubjectClass:0x.*\( config\/=, stores \)/)
+    end
+  end
+end
