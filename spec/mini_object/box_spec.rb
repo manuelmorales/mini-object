aeb1de66068c91da8597d595d31223fc61c0d972
Manuel Morales <manuelmorales@gmail.com>
2015-07-09 11:42:56 +0100

    Config was a bad name choice

--- spec/mini_object/box_spec.rb
+++ spec/mini_object/box_spec.rb
@@ -35,7 +35,7 @@ describe 'Box' do
       end
     }
 
-    stub_const 'Config', Class.new(Box) {
+    stub_const 'Configuration', Class.new(Box) {
       def stores
         @stores ||= { default: 'hash' }
       end
@@ -43,11 +43,11 @@ describe 'Box' do
 
     stub_const 'BoxApp', Class.new(Box) {
       def stores
-        @stores ||= Stores.new config: config.stores
+        @stores ||= Stores.new config: self.config.stores
       end
 
       def config
-        @config ||= Config.new
+        @config ||= Configuration.new
       end
     }
 
